c5500656b3a20c16e02604d8b5571638e6910453
akathedeveloper <adhirajsingh735@gmail.com>
2024-11-04 14:05:12 +0530

    UI Landing Page

---
+++ src/sections/Footer.jsx
@@ -0,0 +1,48 @@
+import { Element } from "react-scroll";
+import { links, logos, socials } from "../constants/index.jsx";
+
+const Footer = () => {
+return (
+    <section>
+        <Element
+            name="download"
+            className="g7 relative pb-5 pt-5 max-lg:pb-24 max-md:py-16"
+        >
+            {/* Footer Section */}
+            <footer>
+                <div className="container py-10">
+                    <div className="flex w-full max-md:flex-col">
+                        <div className="small-compact flex flex-1 flex-wrap items-center justify-center gap-5">
+                            <p className="opacity-70">BitBnB: Made with ❤️ by CipherHives</p>
+                        </div>
+                        <div className="flex items-center justify-center sm:ml-auto">
+                            <p className="legal-after relative mr-9 text-p5 transition-all duration-500 hover:text-p1">
+                                Privacy Policy
+                            </p>
+                            <p className="text-p5 transition-all duration-500 hover:text-p1">
+                                Terms of Use
+                            </p>
+                        </div>
+
+                        <ul className="flex flex-1 justify-center gap-3 max-md:mt-10 md:justify-end">
+                            {socials.map(({ id, url, icon, title }) => (
+                                <li key={id}>
+                                    <a href={url} className="social-icon">
+                                        <img
+                                            src={icon}
+                                            alt={title}
+                                            className="size-1/3 object-contain"
+                                        />
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </footer>
+        </Element>
+    </section>
+);
+};
+
+export default Footer;
